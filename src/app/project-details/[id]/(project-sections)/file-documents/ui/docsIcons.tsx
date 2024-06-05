3434bf677a169cdb7a652de2954ac7ff60a3970e
00jackson <95626883+00jackson@users.noreply.github.com>
2024-06-05 15:21:18 +0530

    Placed File & docs in its correct place

--- src/app/project-details/[id]/(project-sections)/file-documents/ui/docsIcons.tsx
+++ src/app/project-details/[id]/(project-sections)/file-documents/ui/docsIcons.tsx
@@ -17,17 +17,17 @@ export const List:React.FC =() => {
 }
 export const Contracts:React.FC = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="0.9375" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-book-open-text"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/><path d="M6 8h2"/><path d="M6 12h2"/><path d="M16 8h2"/><path d="M16 12h2"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-book-open-text"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/><path d="M6 8h2"/><path d="M6 12h2"/><path d="M16 8h2"/><path d="M16 12h2"/></svg>
     )
 }
 export const Script:React.FC = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="0.9375" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-square-plus"><rect width="18" height="18" x="3" y="3" rx="2"/><path d="M8 12h8"/><path d="M12 8v8"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-square-plus"><rect width="18" height="18" x="3" y="3" rx="2"/><path d="M8 12h8"/><path d="M12 8v8"/></svg>
     )
 }
 export const Sheet:React.FC = () =>{
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.0714285714285714" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-file-spreadsheet"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M8 13h2"/><path d="M14 13h2"/><path d="M8 17h2"/><path d="M14 17h2"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-file-spreadsheet"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M8 13h2"/><path d="M14 13h2"/><path d="M8 17h2"/><path d="M14 17h2"/></svg>
     )
 }
 export const Plus:React.FC = () => {
--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -1,70 +1,66 @@
 'use client'
 
-import React from 'react'
+import { projectdetailsItems } from '@/constants'
+import { useProjectControl } from '@/contexts/ProjectContext'
+import { useSideBarControl } from '@/contexts/SideBarContext'
 import Image from 'next/image'
 import Link from 'next/link'
-import { 
-    CreateProject,
-    Dashboard,
-    File,
- } from './components/Sidebaricons'
 import ProjectButtonGroup from './components/ProjectButtonGroup'
 import SideBarButton from './components/SideBarButton'
 import SideBarCloseButton from './components/SideBarCloseButton'
-import { useSideBarControl } from '@/contexts/SideBarContext'
-import { useProjectControl } from '@/contexts/ProjectContext'
-import { projectdetailsItems } from '@/constants'
+import {
+    CreateProject,
+    Dashboard,
+} from './components/Sidebaricons'
 
 
 
 const SideBar = () => {
 
-    const {isSideBarOpen} = useSideBarControl()
+    const { isSideBarOpen } = useSideBarControl()
 
-    const ProjectDetailsMenu = projectdetailsItems.map((details)=>(
+    const ProjectDetailsMenu = projectdetailsItems.map((details) => (
         <div key={details.title} className='flex flex-col gap-1'>
             <h1 className=" pl-2 text-sm text-gray-400 mt-4 uppercase">
                 {details.title}
             </h1>
             {
-                details.items.map((item)=>(
+                details.items.map((item) => (
                     <li key={item.text} className='list-none'>
-                        <SideBarButton Icon={item.icon} link={item.link} root='project-details' text={item.text}/>
+                        <SideBarButton Icon={item.icon} link={item.link} root='project-details' text={item.text} />
                     </li>
                 ))
             }
         </div>
-        
+
     ))
 
-    const {setProject} = useProjectControl()
+    const { setProject } = useProjectControl()
 
-  return (
-    <aside className={`${isSideBarOpen ? 'translate-x-0' : '-translate-x-80'} overflow-y-auto bg-white shadow-sm fixed inset-0 z-50 h-100vh w-72 transition-transform duration-300 lg:translate-x-0 border border-blue-gray-100`}>
-        <div className="relative">
-            <SideBarCloseButton/>
-            <Link className=" mt-4" href="/">
-                <Image onClick={()=>setProject({id:"", name:""})} className=" mx-auto w-[150px] pt-4 mb-8 " src="/logo-a6299cea.png" width={150} height={78} alt=""/>
-            </Link>
-        </div>
-        <div className='m-4'>
-            <ul className='mb-4 flex flex-col gap-1'>
-                <li>
-                    <SideBarButton Icon={Dashboard} text='dashboard' link='home' root='dashboard'/>
-                </li>
-                <li>
-                    <SideBarButton Icon={CreateProject} text='Create Project' link='new-project' root='dashboard'/>
-                </li>
-                <li>
-                    <SideBarButton Icon={File} text='Files & Documents' link='file&docs' root='dashboard'/>
-                </li>
-            </ul>
-            <ProjectButtonGroup>
-                {ProjectDetailsMenu}
-            </ProjectButtonGroup>
-        </div>
-    </aside>
-  )
+    return (
+        <aside className={`${isSideBarOpen ? 'translate-x-0' : '-translate-x-80'} overflow-y-auto bg-white shadow-sm fixed inset-0 z-50 h-100vh w-72 transition-transform duration-300 lg:translate-x-0 border border-blue-gray-100`}>
+            <div className="relative">
+                <SideBarCloseButton />
+                <Link className=" mt-4" href="/">
+                    <Image onClick={() => setProject({ id: "", name: "" })} className=" mx-auto w-[150px] pt-4 mb-8 " src="/logo-a6299cea.png" width={150} height={78} alt="" />
+                </Link>
+            </div>
+            <div className='m-4'>
+                <ul className='mb-4 flex flex-col gap-1'>
+                    <li>
+                        <SideBarButton Icon={Dashboard} text='dashboard' link='home' root='dashboard' />
+                    </li>
+                    <li>
+                        <SideBarButton Icon={CreateProject} text='Create Project' link='new-project' root='dashboard' />
+                    </li>
+
+                </ul>
+                <ProjectButtonGroup>
+                    {ProjectDetailsMenu}
+                </ProjectButtonGroup>
+            </div>
+        </aside>
+    )
 }
 
 export default SideBar
