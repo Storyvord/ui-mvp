a21ae0d07e65c71f3175dfd1e2bbf9d5201f9a27
Ankit Singh <thakurbharat065@gmail.com>
2024-06-20 21:27:31 +0530

    Solve error

--- src/app/project-details/[id]/page.tsx
+++ src/app/project-details/[id]/page.tsx
@@ -7,36 +7,62 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-const page = async ({params}: {params: {id: string;}}) => {
-  const res = await fetch(`https://sv-aibackend.azurewebsites.net/api/crew/complete-project-details/?project_id=${params.id}`)
-  const projectDetails = await res.json()
+const page = async ({ params }: { params: { id: string } }) => {
+  const res = await fetch(
+    `https://sv-aibackend.azurewebsites.net/api/crew/complete-project-details/?project_id=${params.id}`
+  );
+  const projectDetails = await res.json();
 
   return (
     <div className="flex flex-col items-center  min-h-screen py-2 w-full h-full">
       <Card className="w-full h-full p-6 bg-white shadow-lg rounded-xl overflow-auto">
         <CardHeader>
-          <CardTitle className="text-3xl font-bold text-gray-900 dark:text-white">{projectDetails.project_name}</CardTitle>
+          <CardTitle className="text-3xl font-bold text-gray-900 dark:text-white">
+            {projectDetails.project_name}
+          </CardTitle>
         </CardHeader>
         <CardContent>
           <div className="mt-4">
-            <h2 className="text-lg font-semibold text-gray-700 dark:text-gray-300">Content Type</h2>
-            <p className="text-base text-gray-600 dark:text-gray-200">{projectDetails.content_type}</p>
+            <h2 className="text-lg font-semibold text-gray-700 dark:text-gray-300">
+              Content Type
+            </h2>
+            <p className="text-base text-gray-600 dark:text-gray-200">
+              {projectDetails.content_type}
+            </p>
           </div>
           <div className="mt-4">
-            <h2 className="text-lg font-semibold text-gray-700 dark:text-gray-300">Budget</h2>
-            <p className="text-base text-gray-600 dark:text-gray-200">{projectDetails.budget}</p>
+            <h2 className="text-lg font-semibold text-gray-700 dark:text-gray-300">
+              Budget
+            </h2>
+            <p className="text-base text-gray-600 dark:text-gray-200">
+              {projectDetails.budget}
+            </p>
           </div>
           <div className="mt-4">
-            <h2 className="text-lg font-semibold text-gray-700 dark:text-gray-300">Description</h2>
-            <CardDescription className="text-base text-gray-600 dark:text-gray-200">{projectDetails.description}</CardDescription>
+            <h2 className="text-lg font-semibold text-gray-700 dark:text-gray-300">
+              Description
+            </h2>
+            <CardDescription className="text-base text-gray-600 dark:text-gray-200">
+              {projectDetails.description}
+            </CardDescription>
           </div>
           <div className="mt-4">
-            <h2 className="text-lg font-semibold text-gray-700 dark:text-gray-300">Additional Details</h2>
-            <CardDescription className="text-base text-gray-600 dark:text-gray-200">{projectDetails.additional_details}</CardDescription>
+            <h2 className="text-lg font-semibold text-gray-700 dark:text-gray-300">
+              Additional Details
+            </h2>
+            <CardDescription className="text-base text-gray-600 dark:text-gray-200">
+              {projectDetails.additional_details}
+            </CardDescription>
           </div>
           <div className="mt-4">
-            <h2 className="text-lg font-semibold text-gray-700 dark:text-gray-300">Location</h2>
-            <p className="text-base text-gray-600 dark:text-gray-200">{projectDetails.locations[0]}</p>
+            <h2 className="text-lg font-semibold text-gray-700 dark:text-gray-300">
+              Location
+            </h2>
+            {projectDetails.locations && (
+              <p className="text-base text-gray-600 dark:text-gray-200">
+                {projectDetails.locations[0]}
+              </p>
+            )}
           </div>
         </CardContent>
       </Card>
