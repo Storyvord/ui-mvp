40d8b3ded47715b42152b92a17d87ee0e32e162a
00jackson <95626883+00jackson@users.noreply.github.com>
2024-06-24 20:53:14 +0530

    .

--- src/app/(user-dashboard)/auth/sign-up/page.tsx
+++ src/app/(user-dashboard)/auth/sign-up/page.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
-import Logo from '../Logo/logo.png'; 
+import Logo from '../Logo/logo.png';
 
 interface SignUpFormData {
     email: string;
