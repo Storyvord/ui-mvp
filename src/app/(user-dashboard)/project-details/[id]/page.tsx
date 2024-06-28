559640f838f7dd7dff804c098ff5ca47c8c9644f
Souvik <157105889+s00uvik@users.noreply.github.com>
2024-06-29 01:14:50 +0530

    bug fix

--- src/app/(user-dashboard)/project-details/[id]/page.tsx
+++ src/app/(user-dashboard)/project-details/[id]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useEffect, useState } from "react";
 import {
@@ -8,49 +8,75 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { useCompleteProject, useDeleteProject, useProjectDetails } from "@/lib/react-query/queriesAndMutations";
+import {
+  useCompleteProject,
+  useDeleteProject,
+  useProjectDetails,
+} from "@/lib/react-query/queriesAndMutations";
 import SelectedCrew from "@/components/projectdetails/SelectedCrew";
 import LoadingPage from "@/components/projectdetails/LoadingPage";
 import { Button } from "@/components/ui/button";
-import { AlertDialog, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { MdDelete } from "react-icons/md";
 import { GrStatusGood } from "react-icons/gr";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { useRouter } from "next/navigation";
 import { ReloadIcon } from "@radix-ui/react-icons";
 
-const ProjectPage = ({params}: {params: {id: string;}}) => {
+const ProjectPage = ({ params }: { params: { id: string } }) => {
   // const res = await fetch(`https://sv-aibackend.azurewebsites.net/api/project/complete-project-details/?project_id=${params.id}`)
   // const projectDetails = await res.json()
   // const [projectStatus, setProjectStatus] = useState("");
-  const {data: projectDetails, isLoading: projectDetailsLoading, error} = useProjectDetails(params.id);
-  const {mutateAsync: deleteProject, isLoading:deletingProject} = useDeleteProject();
-  const {mutateAsync: completeProject, isLoading:completingProject} = useCompleteProject(params.id);
+  const {
+    data: projectDetails,
+    isLoading: projectDetailsLoading,
+    error,
+  } = useProjectDetails(params.id);
+  const { mutateAsync: deleteProject, isLoading: deletingProject } =
+    useDeleteProject();
+  const { mutateAsync: completeProject, isLoading: completingProject } =
+    useCompleteProject(params.id);
+
+  console.log(projectDetails);
 
   // useEffect(()=>{
   //     setProjectStatus(projectDetails ? projectDetails.status : "");
   // }, [projectDetails])
   const router = useRouter();
-  const handleDeleteProject = async ()=>{
-    await deleteProject({project_id: params.id});
-    router.push('/dashboard/home');
-  }
+  const handleDeleteProject = async () => {
+    await deleteProject({ project_id: params.id });
+    router.push("/dashboard/home");
+  };
 
-  const handleCompleteProject = async ()=>{
-    await completeProject({project_id: params.id});
+  const handleCompleteProject = async () => {
+    await completeProject({ project_id: params.id });
     // setProjectStatus("COMPLETED");
+  };
+
+  if (projectDetailsLoading) {
+    return <LoadingPage />;
   }
 
-  if(projectDetailsLoading){
-    return(
-      <LoadingPage />
-    )
-  }
-
-  if(error){
-    return(
-      <div className="w-full text-center text-gray-700">Failed to fetch project details</div>
-    )
+  if (error) {
+    return (
+      <div className="w-full text-center text-gray-700">
+        Failed to fetch project details
+      </div>
+    );
   }
 
   return (
@@ -74,51 +100,79 @@ const ProjectPage = ({params}: {params: {id: string;}}) => {
             <AlertDialog>
               <AlertDialogTrigger asChild>
                 <div>
-                  <MdDelete className="w-5 h-5 sm:w-6 sm:h-6 text-red-500 hover:text-red-400 cursor-pointer"/>
-                </div> 
+                  <MdDelete className="w-5 h-5 sm:w-6 sm:h-6 text-red-500 hover:text-red-400 cursor-pointer" />
+                </div>
               </AlertDialogTrigger>
               <AlertDialogContent>
                 <AlertDialogHeader>
                   <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
                   <AlertDialogDescription>
-                    This action cannot be undone. This will permanently delete the project and 
-                    remove your project data from our servers.
+                    This action cannot be undone. This will permanently delete
+                    the project and remove your project data from our servers.
                   </AlertDialogDescription>
                 </AlertDialogHeader>
                 <AlertDialogFooter>
                   <AlertDialogCancel>Cancel</AlertDialogCancel>
-                  <Button onClick={handleDeleteProject} className={`${deletingProject? "disabled" : ""}`}>
+                  <Button
+                    onClick={handleDeleteProject}
+                    className={`${deletingProject ? "disabled" : ""}`}
+                  >
                     {deletingProject ? "Deleting..." : "Delete"}
                   </Button>
                 </AlertDialogFooter>
               </AlertDialogContent>
             </AlertDialog>
             <div className="flex items-center gap-2">
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger disabled= {projectDetails?.status==="COMPLETED"}>
-                  <div onClick={handleCompleteProject} > 
-                    {
-                      completingProject? <ReloadIcon className="h-5 w-5 animate-spin text-bold text-gray-500" /> : (
-                        <GrStatusGood className={`w-5 h-5 sm:w-6 sm:h-6 cursor-pointer ${projectDetails?.status==="COMPLETED" ? "text-green-500" : "text-gray-500"}`}/>
-                      )
-                    }
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger
+                    disabled={projectDetails?.status === "COMPLETED"}
+                  >
+                    <div onClick={handleCompleteProject}>
+                      {completingProject ? (
+                        <ReloadIcon className="h-5 w-5 animate-spin text-bold text-gray-500" />
+                      ) : (
+                        <GrStatusGood
+                          className={`w-5 h-5 sm:w-6 sm:h-6 cursor-pointer ${
+                            projectDetails?.status === "COMPLETED"
+                              ? "text-green-500"
+                              : "text-gray-500"
+                          }`}
+                        />
+                      )}
+                    </div>
+                  </TooltipTrigger>
+                  <TooltipContent
+                    className={`${
+                      projectDetails?.status === "COMPLETED" ? "hidden" : ""
+                    } bg-transparent shadow-none border-none`}
+                  >
+                    <p className="text-sm text-green-500 bold">
+                      Mark Project As Completed
+                    </p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+
+              <div>
+                {projectDetails?.status === "INITIALIZED" && (
+                  <div className="text-sm sm:text-base text-gray-500 font-bold">
+                    INITIALIZED
                   </div>
-                </TooltipTrigger>
-                <TooltipContent className={`${projectDetails?.status==="COMPLETED" ? "hidden" : ""} bg-transparent shadow-none border-none`}>
-                  <p className="text-sm text-green-500 bold">Mark Project As Completed</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-            
-            <div>
-              {projectDetails?.status === "INITIALIZED" && <div className="text-sm sm:text-base text-gray-500 font-bold">INITIALIZED</div>}
-              {projectDetails?.status === "COMPLETED" && <div className="text-sm sm:text-base text-green-500 font-bold">COMPLETED</div>}
-              {projectDetails?.status === "PLANNING" && <div className="text-sm sm:text-base text-yellow-500 font-bold">PLANNING</div>}
+                )}
+                {projectDetails?.status === "COMPLETED" && (
+                  <div className="text-sm sm:text-base text-green-500 font-bold">
+                    COMPLETED
+                  </div>
+                )}
+                {projectDetails?.status === "PLANNING" && (
+                  <div className="text-sm sm:text-base text-yellow-500 font-bold">
+                    PLANNING
+                  </div>
+                )}
+              </div>
             </div>
-            </div>
-            
-          </div> 
+          </div>
           <CardTitle className="sm:text-3xl font-bold text-gray-900 dark:text-white float-left">
             {projectDetails?.project_name}
           </CardTitle>
@@ -126,13 +180,13 @@ const ProjectPage = ({params}: {params: {id: string;}}) => {
         <CardContent className="font-sans p-0 flex flex-col gap-2 mt-4">
           <div className="flex flex-wrap items-center gap-2">
             <h2 className="text-base sm:text-lg font-semibold text-gray-700 dark:text-gray-300">
-              Content Type: 
+              Content Type:
             </h2>
             <p className="text-base text-gray-600 dark:text-gray-200">
               {projectDetails?.content_type}
             </p>
           </div>
-          <div >
+          <div>
             <h2 className="text-base sm:text-lg font-semibold text-gray-700 dark:text-gray-300">
               Description
             </h2>
@@ -149,10 +203,16 @@ const ProjectPage = ({params}: {params: {id: string;}}) => {
             </p>
           </div>
           <div className="flex items-center gap-2 flex-wrap">
-            <h2 className="text-base sm:text-lg font-semibold text-gray-700 dark:text-gray-300">Location: </h2>
-            {
-              projectDetails?.location_details?.length>0 && (<div className="text-base text-gray-600 dark:text-gray-200 flex">{projectDetails?.location_details.map((item:any)=>(<p key={item.location}>{item.location}, &nbsp;</p>))}</div>)
-            }
+            <h2 className="text-base sm:text-lg font-semibold text-gray-700 dark:text-gray-300">
+              Location:{" "}
+            </h2>
+            {projectDetails?.location_details?.length > 0 && (
+              <div className="text-base text-gray-600 dark:text-gray-200 flex">
+                {projectDetails?.location_details.map((item: any) => (
+                  <p key={item.location}>{item.location}, &nbsp;</p>
+                ))}
+              </div>
+            )}
           </div>
           <div>
             <h2 className="text-base sm:text-lg font-semibold text-gray-700 dark:text-gray-300">
@@ -165,7 +225,7 @@ const ProjectPage = ({params}: {params: {id: string;}}) => {
         </CardContent>
       </Card>
       <div className="w-full h-auto mt-5">
-            <SelectedCrew crews={projectDetails?.selected_crews_set}/>
+        <SelectedCrew crews={projectDetails?.suggested_crews_set} />
       </div>
     </div>
   );
--- src/components/projectdetails/SelectedCrew.tsx
+++ src/components/projectdetails/SelectedCrew.tsx
@@ -1,34 +1,35 @@
-import { SelectedCrewMember } from '@/types'
-import { FC } from 'react'
-import CrewCard from './CrewCard'
+import { SelectedCrewMember } from "@/types";
+import { FC } from "react";
+import CrewCard from "./CrewCard";
 
 interface SelectedCrewProps {
-    crews:{
-        crew_member: SelectedCrewMember,
-        preferred_because?:string
-    }[]
-    
+  crews: {
+    crew_member: SelectedCrewMember;
+    preferred_because?: string;
+  }[];
 }
 
-const SelectedCrew: FC<SelectedCrewProps> = ({crews}) => {
-  return <div>
-    <h1 className='text-base sm:text-xl font-semibold font-sans'>Selected Crew Members</h1>
-    <div className='max-w-full overflow-x-auto md:overflow-x-hidden mb-[5px] pt-2 pl-2 hover:mb-0 md:hover:overflow-x-auto py-4'>
-        {
-            crews.length === 0 ? <p className='text-sm text-center text-gray-500'>No crew members selected</p>: (
-                <div className='flex gap-4'>
-                    {
-                        crews.map((crew) => (
-                            <CrewCard key={crew.crew_member.id} crew={crew}/>
-                        ))
-                    }
-                </div>
-            )
-        }
-        
+const SelectedCrew: FC<SelectedCrewProps> = ({ crews }) => {
+  return (
+    <div>
+      <h1 className="text-base sm:text-xl font-semibold font-sans">
+        Suggested Crew Members
+      </h1>
+      <div className="max-w-full overflow-x-auto md:overflow-x-hidden mb-[5px] pt-2 pl-2 hover:mb-0 md:hover:overflow-x-auto py-4">
+        {crews.length === 0 ? (
+          <p className="text-sm text-center text-gray-500">
+            No crew members selected
+          </p>
+        ) : (
+          <div className="flex gap-4">
+            {crews.map((crew) => (
+              <CrewCard key={crew.crew_member.id} crew={crew} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
-    
-  </div>
-}
+  );
+};
 
-export default SelectedCrew
+export default SelectedCrew;
