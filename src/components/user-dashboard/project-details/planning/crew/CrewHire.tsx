8e048be8e627b8b04cda14dc7cd6c1f74b45153f
souvik <souvik@storyvord.com>
2024-08-06 09:26:54 +0530

    update crew page

--- src/components/user-dashboard/project-details/planning/crew/CrewHire.tsx
+++ src/components/user-dashboard/project-details/planning/crew/CrewHire.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { CiLock } from "react-icons/ci";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Input } from "@/components/ui/input";
 import {
   Accordion,
@@ -11,40 +9,20 @@ import {
 } from "@/components/ui/accordion";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import ExternalContactDialog from "./ExternalContactDialog";
+import CrewHireNavBar from "./CrewHireNavBar";
+
 
 const CrewHire = () => {
   const [openDialog, setOpenDialog] = useState(false);
   const [openDialogExternalContact, setOpenDialogExternalContact] = useState(false);
   return (
     <div>
-      <nav className="mx-auto flex gap-4 justify-end mr-8">
-        <Popover>
-          <PopoverTrigger>
-            <Button className=" mr-4 font-semibold">Add Crew</Button>
-          </PopoverTrigger>
-          <PopoverContent>
-            <h1
-              className=" p-1 pl-2 hover:bg-gray-200 rounded cursor-pointer"
-              onClick={() => setOpenDialog(!openDialog)}
-            >
-              Invite Crew
-            </h1>
-            <h1 onClick={() => setOpenDialogExternalContact(!openDialogExternalContact)} className=" mt-2 p-1 pl-2 hover:bg-gray-200 rounded cursor-pointer">
-              Add external contact
-            </h1>
-          </PopoverContent>
-        </Popover>
-
-        <Popover>
-          <PopoverTrigger className=" flex gap-2 p-1 border-none rounded-lg px-2 item-center font-semibold bg-white border text-lg">
-            <CiLock className=" w-6 h-6" />
-            Access Rights
-          </PopoverTrigger>
-          <PopoverContent>
-            <Input placeholder="enter email" />
-          </PopoverContent>
-        </Popover>
-      </nav>
+      <CrewHireNavBar
+        openDialog={openDialog}
+        setOpenDialog={setOpenDialog}
+        openDialogExternalContact={openDialogExternalContact}
+        setOpenDialogExternalContact= {setOpenDialogExternalContact}
+      />
       <main className="mt-12 flex flex-col gap-8">
         <Accordion type="single" collapsible className="bg-white p-4 rounded-md shadow-md">
           <AccordionItem value="item-1">
@@ -81,7 +59,7 @@ const CrewHire = () => {
           </DialogHeader>
         </DialogContent>
       </Dialog>
-      
+
       <ExternalContactDialog
         openDialogExternalContact={openDialogExternalContact}
         setOpenDialogExternalContact={setOpenDialogExternalContact}
