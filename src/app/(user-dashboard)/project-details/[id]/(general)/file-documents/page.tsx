2ea0d088a76ff2895ea9ee2cd6d1f01b6fbbb9be
souvik <souvik@storyvord.com>
2024-08-09 15:30:57 +0530

    file api integration pending

--- src/app/(user-dashboard)/project-details/[id]/(general)/file-documents/page.tsx
+++ src/app/(user-dashboard)/project-details/[id]/(general)/file-documents/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import { SubmitHandler } from "react-hook-form";
 import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useParams, useRouter } from "next/navigation";
-import FileManagement from "@/components/user-dashboard/project-details/general/file-documents/FileManagement";
 import RoomForm from "@/components/user-dashboard/project-details/general/file-documents/RoomForm";
 import RoomCard from "@/components/user-dashboard/project-details/general/file-documents/RoomCard";
+import { useGetAllFileDocumentRooms } from "@/lib/react-query/queriesAndMutations/file";
 
 type FormData = {
   roomName: string;
@@ -21,51 +21,18 @@ type RoomDataType = {
 };
 
 const FileSection: FC = () => {
-  const [isHome, setIsHome] = useState(true);
-  const [currentRoomId, setCurrentRoomId] = useState<string | null>(null);
   const [showForm, setShowForm] = useState(false);
   const [createdRooms, setCreatedRooms] = useState<RoomDataType[]>([]);
   const [loading, setLoading] = useState(false);
 
+  const { id: projectId }:{id:string} = useParams();
+  const {data} = useGetAllFileDocumentRooms(projectId)
+  console.log(data)
+
   const router = useRouter();
-  const { id: projectId } = useParams();
-
-  useEffect(() => {
-    const storedRooms = localStorage.getItem("createdRooms");
-    if (storedRooms) {
-      setCreatedRooms(JSON.parse(storedRooms));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("createdRooms", JSON.stringify(createdRooms));
-  }, [createdRooms]);
-
-  const onSubmit: SubmitHandler<FormData> = (data) => {
-    setLoading(true);
-    setTimeout(() => {
-      const roomId = data.roomName;
-      const room: RoomDataType = {
-        id: roomId,
-        title: data.roomName,
-        description: data.roomDesc,
-      };
-      setCreatedRooms([...createdRooms, room]);
-      setLoading(false);
-      setShowForm(false);
-    }, 1000);
-  };
 
   const handleCardClick = (roomId: string) => {
-    // setIsHome(false);
-    // setCurrentRoomId(roomId);
     router.push(`/project-details/${projectId}/file-documents/${roomId}`);
-  };
-
-  const handleBackToHome = () => {
-    setIsHome(true);
-    setCurrentRoomId(null);
-    router.push(`/project-details/${projectId}/file-documents`);
   };
 
   return (
@@ -80,29 +47,14 @@ const FileSection: FC = () => {
         </Button>
       </div>
 
-      {isHome ? (
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 mt-5">
-          {createdRooms.map((room, index) => (
-            <RoomCard
-              key={index}
-              room={room}
-              onClick={handleCardClick}
-            />
-          ))}
-        </div>
-      ) : (
-        <div className="p-4">
-          <button onClick={handleBackToHome} className="mb-4 text-blue-500">
-            Back to File Management
-          </button>
-          {currentRoomId && <FileManagement roomId={currentRoomId} />}
-        </div>
-      )}
-
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 mt-5">
+        {createdRooms.map((room, index) => (
+          <RoomCard key={index} room={room} onClick={handleCardClick} />
+        ))}
+      </div>
       <RoomForm
         open={showForm}
         onClose={() => setShowForm(false)}
-        onSubmit={onSubmit}
         loading={loading}
       />
     </section>
