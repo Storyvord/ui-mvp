5513ba6e6c7dbbdf98b4a6ba3df71e46d090b6b9
souvik <souvik@storyvord.com>
2024-08-11 12:15:10 +0530

    create project section for crew

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,21 +1,22 @@
+import clsx from "clsx";
 import { FC } from "react";
 
 interface TabsProps {
   activeTab: string;
   setActiveTab: (tab: string) => void;
-  tabs: string[]
+  tabs: string[];
+  className?: string;
 }
 
-const Tabs: FC<TabsProps> = ({ activeTab, setActiveTab, tabs }) => {
-
+const Tabs: FC<TabsProps> = ({ activeTab, setActiveTab, tabs, className }) => {
   return (
-    <div className="border-b border-gray-200 bg-white overflow-x-auto pl-4">
+    <div className={clsx("border-b border-gray-200 overflow-x-auto", className)}>
       <nav className="flex space-x-4" aria-label="Tabs">
         {tabs.map((tab) => (
           <button
             key={tab}
             onClick={() => setActiveTab(tab)}
-            className={`whitespace-nowrap  px-1 py-3 text-xl font-semibold ${
+            className={`whitespace-nowrap  px-1 py-3 font-semibold ${
               activeTab === tab
                 ? "from-gray-900 border-b-2 border-gray-700 "
                 : " text-gray-500 hover:text-gray-700"
