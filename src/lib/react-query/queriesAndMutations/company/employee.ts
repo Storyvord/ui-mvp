175986cf070a95b4549765e5035de4f7e7ef5bc1
souvik <souvik@storyvord.com>
2024-08-27 12:55:29 +0530

    api integration pending

---
+++ src/lib/react-query/queriesAndMutations/company/employee.ts
@@ -0,0 +1,60 @@
+import {
+  acceptCompanyInvitation,
+  getCompanyInvitations,
+  getOnBoardedEmployeeList,
+  rejectCompanyInvitation,
+  sentInvitationToEmployee,
+} from "@/lib/api/company/employee";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+
+export const useSentInvitationToEmployee = () => {
+  return useMutation({
+    mutationFn: sentInvitationToEmployee,
+    onSuccess: (data) => {
+      return data;
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+};
+
+export const useGetOnBoardedEmployeeList = () => {
+  return useQuery({
+    queryKey: ["getOnBoardedEmployeeList"],
+    queryFn: () => getOnBoardedEmployeeList(),
+  });
+};
+
+export const useGetCompanyInvitations = () => {
+  return useQuery({
+    queryKey: ["getCompanyInvitations"],
+    queryFn: getCompanyInvitations,
+  });
+};
+
+export const useAcceptCompanyInvitation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: acceptCompanyInvitation,
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ["getCompanyInvitations", "getOnBoardedEmployeeList"],
+      });
+      return data;
+    },
+  });
+};
+
+export const useRejectCompanyInvitation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: rejectCompanyInvitation,
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ["getCompanyInvitations", "getOnBoardedEmployeeList"],
+      });
+      return data;
+    },
+  });
+};
