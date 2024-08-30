016c43a32934f09042515b97796a3583b3063bf7
souvik <souvik@storyvord.com>
2024-08-30 18:03:26 +0530

    integrate company settings api

---
+++ src/lib/react-query/queriesAndMutations/company/settings.ts
@@ -0,0 +1,31 @@
+import { useToast } from "@/components/ui/use-toast";
+import { getCompanySettings, updateCompanySettings } from "@/lib/api/company/settings";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+
+export const useGetCompanySettings = () => {
+  return useQuery({
+    queryKey: ["getCompanySettings"],
+    queryFn: getCompanySettings,
+  });
+};
+
+
+export const useUpdateCompanySettings = () => {
+  const { toast } = useToast();
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: updateCompanySettings,
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ["getCompanySettings"],
+      });
+      toast({
+        title: "Successfully update company settings"
+      })
+      return data;
+    },
+    onError: (error) => {
+      return;
+    },
+  });
+};
