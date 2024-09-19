f009c65d9e0af97b665a318c78f6dda1997504f8
PiyushKumar11 <85399697+PiyushKumar11@users.noreply.github.com>
2024-09-19 15:07:16 +0530

    Resolved logout issues

--- src/app/(user-dashboard)/layout.tsx
+++ src/app/(user-dashboard)/layout.tsx
@@ -4,29 +4,42 @@ import SideBar from "@/components/sidebar/SideBar";
 import { Toaster } from "@/components/ui/toaster";
 import { FC, ReactNode, Suspense } from "react";
 
+import Chatbot from "@/components/chat/Chatbot";
 import ProjectContextProvider from "@/context/ProjectContext";
 import SideBarContextProvider from "@/context/SideBarContext";
 import UserContextProvider from "@/context/UserContext";
-import Chatbot from "@/components/chat/Chatbot";
+import Cookies from "js-cookie";
 import Loading from "./loading";
+import { useGetUserDetails } from "@/lib/react-query/queriesAndMutations/auth/auth";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout: FC<LayoutProps> = ({ children }) => {
+  const token = Cookies.get("accessToken");
+  if (!token) window.location.replace("/auth/sign-in");
+
+  const { data: userDetails, isLoading } = useGetUserDetails();
+
   return (
     <UserContextProvider>
       <div className="w-full min-h-screen bg-[#eceff180] relative">
         <SideBarContextProvider>
           <ProjectContextProvider>
-            <SideBar />
-            <div className="lg:ml-72 font-sans">
-              <NavBar />
-              <Suspense fallback={<Loading />}>{children}</Suspense>
-            </div>
-            <Toaster />
-            <Chatbot />
+            {userDetails && token && !isLoading ? (
+              <>
+                <SideBar />
+                <div className="lg:ml-72 font-sans">
+                  <NavBar />
+                  <Suspense fallback={<Loading />}>{children}</Suspense>
+                </div>
+                <Toaster />
+                <Chatbot />
+              </>
+            ) : (
+              <Loading />
+            )}
           </ProjectContextProvider>
         </SideBarContextProvider>
       </div>
