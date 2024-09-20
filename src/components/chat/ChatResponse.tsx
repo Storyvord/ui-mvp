f57416bf0912d6b0a3059c1e4f87c6d104f102fd
PiyushKumar11 <85399697+PiyushKumar11@users.noreply.github.com>
2024-09-20 17:12:06 +0530

    Moved chatbot to sockets

--- src/components/chat/ChatResponse.tsx
+++ src/components/chat/ChatResponse.tsx
@@ -15,15 +15,13 @@ export const ChatResponse: React.FC<ChatResponseProps> = ({
   showLoading,
 }) => {
   return (
-    <div className="flex w-10/12 bg-white">
-      <div className="grid place-items-center bg-gradient-to-r from-[#1A68FF] to-[#009185] rounded-sm mr-2 w-[3rem] h-[3rem] p-2">
-        {creation}
-      </div>
+    <div className="flex w-10/12 ">
+      <div className="grid place-items-center bg-gray-300 rounded-full mr-2 w-[3rem] h-[3rem] p-2"></div>
 
       {isLoading && showLoading && <Loader />}
 
       {data && (
-        <div className="flex flex-col gap-2 w-full p-4 rounded-md bg-gray-200">
+        <div className="flex flex-col gap-2 w-full p-4 rounded-tl-none rounded-tr-[16px] rounded-br-[16px] rounded-bl-[16px] bg-chatbot-color1 text-white">
           <ChatTextResponse data={data?.data} />
         </div>
       )}
--- src/components/chat/ChatbotQuestions.tsx
+++ src/components/chat/ChatbotQuestions.tsx
@@ -8,7 +8,9 @@ interface ChatbotQuestionsProps {
 export const ChatbotQuestions: React.FC<ChatbotQuestionsProps> = ({ item }) => {
   return (
     <div className="flex align-middle justify-end gap-2 p-1 w-9/12 ">
-      <p className="bg-#292935 p-4 rounded-md bg-gray-200">{item?.data}</p>
+      <p className="p-4 rounded-md bg-black rounded-tr-none rounded-tl-[16px] rounded-br-[16px] rounded-bl-[16px] text-white">
+        {item?.data}
+      </p>
       <CgProfile className=" w-8 h-8" />
     </div>
   );
