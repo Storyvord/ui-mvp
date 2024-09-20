2e37eb124026a12cc87dbca2716337ac745ed568
PiyushKumar11 <85399697+PiyushKumar11@users.noreply.github.com>
2024-09-20 17:26:00 +0530

    Moved chatbot to sockets

--- src/components/chat/ChatbotDetails.tsx
+++ src/components/chat/ChatbotDetails.tsx
@@ -6,7 +6,7 @@ import { ChatbotSearch } from "./ChatbotSearch";
 import Image from "next/image";
 
 interface ChatbotDetailsProps {
-  conversation: Array<Conversation> | [];
+  conversation: Array<ChatConversation> | [];
   sendMessage: (incomingQuestion: string) => void;
 }
 
@@ -22,7 +22,7 @@ export const ChatbotDetails: React.FC<ChatbotDetailsProps> = ({ conversation, se
 
   // Filter the results based on search
   let filteredConversation: Array<number> = [];
-  conversation.forEach((item: Conversation, index) => {
+  conversation.forEach((item: ChatConversation, index) => {
     if (search.length > 0 && item?.queryType === "question" && item?.data?.indexOf(search) > -1) {
       filteredConversation.push(index);
     }
@@ -41,17 +41,17 @@ export const ChatbotDetails: React.FC<ChatbotDetailsProps> = ({ conversation, se
     <div
       className={
         expanded
-          ? "fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2  flex flex-col w-[70vw] h-[80vh] rounded-lg bg-white overflow-hidden border border-gray-300"
+          ? "fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2  flex flex-col w-[85vw] h-[85vh] rounded-lg bg-white overflow-hidden border border-gray-300"
           : "flex flex-col w-[30vw] h-[70vh] rounded-lg bg-white overflow-hidden border border-gray-300"
       }
     >
-      <div className="flex align-middle justify-between  mx-2 pt-4">
+      <div className="flex align-middle justify-between  mx-2 p-2 h-14">
         <Image className="w-[150px]" src="/storyvord-ai.svg" width={100} height={10} alt="" />
         <button onClick={() => setExpanded(!expanded)}>
           <Image className="w-[30px]" src={"/circle-expand.svg"} width={50} height={10} alt="" />
         </button>
       </div>
-      <div className="p-3 overflow-x-hidden bg-gray-100">
+      <div className="p-3 overflow-x-hidden bg-gray-100 h-[100%]">
         <ChatResponse data={initialResponse} isLoading={false} error={""} showLoading={false} />
         {filteredConversation?.map((item, key) => {
           return (
@@ -73,7 +73,7 @@ export const ChatbotDetails: React.FC<ChatbotDetailsProps> = ({ conversation, se
         })}
         <div ref={messagesEndRef} />
       </div>
-      <div>
+      <div className="h-14">
         <ChatbotSearch suggestedQueries={[]} isLoading={false} sendMessage={sendMessage} />
       </div>
     </div>
