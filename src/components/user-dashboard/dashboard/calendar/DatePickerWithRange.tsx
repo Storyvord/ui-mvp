90f99505029f924434455ad0d91d1c9de3855e20
Souvik <souvik@storyvord.com>
2024-10-18 17:13:34 +0530

    implement calendar section

--- src/components/user-dashboard/dashboard/calendar/DatePickerWithRange.tsx
+++ src/components/user-dashboard/dashboard/calendar/DatePickerWithRange.tsx
@@ -1,7 +1,6 @@
-// src/components/DatePickerWithRange.tsx
 "use client";
 
-import * as React from "react";
+import React from "react";
 import { addDays } from "date-fns";
 import { Calendar } from "@/components/ui/calendar";
 
@@ -9,11 +8,7 @@ interface DatePickerWithRangeProps extends React.HTMLAttributes<HTMLDivElement> 
   onSelectRange: (range: any | undefined) => void;
 }
 
-export function DatePickerWithRange({
-  onSelectRange,
-  className,
-  ...props
-}: DatePickerWithRangeProps) {
+const DatePickerWithRange = ({ onSelectRange, className, ...props }: DatePickerWithRangeProps) => {
   const [date, setDate] = React.useState<any | undefined>({
     from: new Date(),
     to: addDays(new Date(), 7),
@@ -28,7 +23,7 @@ export function DatePickerWithRange({
   return (
     <div className={`bg-white rounded-lg ${className}`} {...props}>
       <Calendar
-        initialFocus
+        // initialFocus
         mode="range"
         defaultMonth={date?.from}
         selected={date}
@@ -37,4 +32,6 @@ export function DatePickerWithRange({
       />
     </div>
   );
-}
+};
+
+export default DatePickerWithRange;
