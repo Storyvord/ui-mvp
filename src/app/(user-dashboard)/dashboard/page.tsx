b3ea25e76bd9aca83d7348c3964bb7ac0a3b3a24
Souvik <souvik@storyvord.com>
2024-11-13 02:22:23 +0530

    add previous Project section

--- src/app/(user-dashboard)/dashboard/page.tsx
+++ src/app/(user-dashboard)/dashboard/page.tsx
@@ -1,26 +1,40 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import Posting from "@/components/user-dashboard/dashboard/posting/Posting";
 import Project from "@/components/user-dashboard/dashboard/project/Project";
 import Tasks from "@/components/user-dashboard/dashboard/tasks/Tasks";
 import MyNetwork from "@/components/user-dashboard/dashboard/network/MyNetwork";
 import CalendarSection from "@/components/user-dashboard/dashboard/calendar/CalendarSection";
+import PastProjects from "@/components/dashboardHome/PastProjects";
+import { useGetProjects } from "@/lib/react-query/queriesAndMutations/project";
+import { Project as ProjectType } from "@/types/project";
 
 const Dashboard = () => {
-  const componentRef = useRef<HTMLDivElement>(null);
+  const { data: projects, isPending, isError } = useGetProjects();
+  const [pastProjects, setPastProjects] = useState<ProjectType[]>([]);
+  const [onGoingProjects, setOngoingProjects] = useState<ProjectType[]>([]);
 
   useEffect(() => {
-    if (componentRef.current) {
-      componentRef.current.scrollTop = 0;
+    if (projects) {
+      const filteredPastProjects = projects.filter((project: ProjectType) =>
+        ["COMPLETED", "CANCELLED", "POST_PRODUCTION"].includes(project.status)
+      );
+      const filteredOngoingProjects = projects.filter(
+        (project: ProjectType) =>
+          !["COMPLETED", "CANCELLED", "POST_PRODUCTION"].includes(project.status)
+      );
+
+      setPastProjects(filteredPastProjects);
+      setOngoingProjects(filteredOngoingProjects);
     }
-  }, []);
+  }, [projects]);
 
   return (
-    <main className=" p-4 sm:py-6 sm:px-10" ref={componentRef}>
+    <main className=" p-4 sm:py-6 sm:px-10 border border-red-500">
       <h1 className="text-xl md:text-2xl font-semibold">Dashboard</h1>
       <div className=" grid grid-cols-1 md:grid-cols-4">
         <section className="md:col-span-3 h-full py-3 pr-2">
-          <Project />
+          <Project onGoingProjects={onGoingProjects} />
           <Posting />
           <CalendarSection />
         </section>
@@ -29,6 +43,7 @@ const Dashboard = () => {
           <MyNetwork />
         </section>
       </div>
+      <PastProjects projects={pastProjects} isLoading={isPending} isError={isError} />
     </main>
   );
 };
